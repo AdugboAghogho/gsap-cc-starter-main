f8c1759dbf254e39b33687e9cadde1ddbfb59d97
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-18 16:59:25 +0100

    uyg

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -71,7 +71,13 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          if (timeline.paused()) {
+            timeline.play();
+          } else {
+            timeline.pause();
+          }
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
