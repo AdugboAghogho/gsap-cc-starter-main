a831b5d44d434b510cbdcbca0165aed33fa951a1
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-18 16:03:21 +0100

    green box

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.to('#green-box', {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotate: 360,
+      duration: 2,
+      ease: 'power1.inOut',
+    })
+  }, []);
 
   return (
     <main>
